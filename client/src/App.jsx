c86be739b0bb0177a7c9622d5a2b8a16f284250b
boyangx <boyangx@umich.edu>
2021-12-20 22:51:47 -0800

    added server side code but stream API is no longer free

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import "./App.css";
 
 const cookies = new Cookies();
 
-const apiKey = "ytdmphnk8sqd";
+const apiKey = "8adasnu3p7yw";
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
