f01807b0bc93bd81b7a5b293e94921df6e44748d
boyangx <boyangx@umich.edu>
2022-01-08 11:40:44 -0800

    deleted second-layed git

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
-// const twilioClient = require("twilio")(accountSid, authToken);
+// const twilioClient = require("twilio")(accountSid, authToken); remove twilio for now
 
 app.use(cors());
 app.use(express.json());
